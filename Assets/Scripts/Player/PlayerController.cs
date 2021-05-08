7d0187e83c3773b903a89bc377309a96dc0024a5
Marcus <marcus.lyth@gmail.com>
2021-05-08 10:27:54 +0200

    Working on movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [Header("Components")]
+        [SerializeField]
+        private CharacterController characterController;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Movement Settings")]
+        [SerializeField, Min(0.001f)]
+        private float baseMovementSpeed;
+
+        [SerializeField, Min(0.001f)]
+        private float maxSpeed = 10f;
+
+        [SerializeField]
+        private float acceleration = 5f;
+
+        [SerializeField, Tooltip("How quickly the spark slows down while braking")]
+        private float brakeSpeed = 10f;
+
+        [SerializeField, Tooltip("How quickly the spark slows down when no input is detected")]
+        private float deceleration = 4f;
+
+        [SerializeField, Range(0f, 1f)]
+        private float sideFriction = 0.95f;
+
+        [SerializeField, Tooltip("How tightly the kart can turn left or right.")]
+        public float steering = 5f;
+
+        [SerializeField, Tooltip("Additional gravity for when the kart is in the air.")]
+        public float jumpGravity = 1f;
+
+        private new Transform transform;
+        private Vector3 _movementVector;
+
+        private void Start()
+        {
+            transform = base.transform;
+        }
+
+        private void Update()
+        {
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            var movementVector = transform.forward + Physics.gravity + _movementVector * baseMovementSpeed;
+            characterController.Move(movementVector * Time.deltaTime);
+        }
+
+        public void OnMovementInput(InputAction.CallbackContext context)
+        {
+            var input = context.ReadValue<Vector2>();
+            _movementVector = new Vector3(input.x, 0, input.y);
+        }
     }
 }
